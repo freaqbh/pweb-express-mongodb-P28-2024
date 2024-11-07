3992f2f0d0200c5d9760c8a328ba386de9fdaa7a
freaqbh <man.in.the.middle1312@gmail.com>
2024-11-07 23:20:51 +0700

    modify

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // index.ts
 import express from "express";
 import dbConnection from "./db-connection"; 
@@ -15,19 +14,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-=======
-import express, { Request, Response } from "express";
-
-const app = express();
-
-app.use(express.json());
-
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).send("Server is up and running 💫");
-});
-
-const PORT = 4000;
-app.listen(PORT, () => {
-  console.log(`Express is running on Port ${PORT}`);
-});
->>>>>>> main
