8c3ea8c15687c448f9d76b3de6ca176eac8ba49e
DaffaEA <daffarajendra191@gmail.com>
2024-11-08 22:01:00 +0700

    aa

---
+++ src/routes/mechanism.routes.ts
@@ -0,0 +1,10 @@
+import { Express } from "express";
+const router = require('express').Router();
+
+import { auth } from "middleware/auth";
+import { mechanismcontroller } from "controllers/mechanism.controller";
+
+router.post("/borrow/:id", auth, mechanismcontroller.borrow);
+router.post("/return/:id", auth, mechanismcontroller.returnBook);
+
+export default router;
